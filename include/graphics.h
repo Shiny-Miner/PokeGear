6f31d985f25bd7ca67532592c8239bdcc6abbabd
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-09-01 05:06:58 +0530

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -4549,4 +4549,14 @@ extern const u16 gPokeStorageInterface_NoDisplayMon_Pal[];
 extern const u16 gDexScreen_TopMenuIconPals_AtoZ[];
 extern const u32 gDexScreen_TopMenuIconTiles_AtoZ[];
 
+
+// Pokegear/Clock
+extern const u32 gSetClock_Gfx[];
+extern const u16 gSetClock_Pal[];
+extern const u32 gSetClock_Map[];
+extern const u32 gPokegear_GridMap[];
+extern const u8 gPhoneCall_WindowGfx[0x100];
+extern const u16 gPhoneCall_WindowPal[16];
+extern const u32 gPhoneCall_IconGfx[];
+
 #endif //GUARD_GRAPHICS_H
