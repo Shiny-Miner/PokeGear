1f24c239eea828a94d861705032b7967a4bd3322
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-09-01 13:05:34 +0530

    Create graphics.c

---
+++ src/graphics.c
@@ -0,0 +1,10 @@
+#include "../include/global.h"
+
+const u32 gSetClock_Gfx[] = INCBIN_U32("graphics/wallclock/setclock.4bpp.lz");
+const u16 gSetClock_Pal[] = INCBIN_U16("graphics/wallclock/setclock.gbapal");
+const u32 gSetClock_Map[] = INCBIN_U32("graphics/wallclock/setclock.bin.lz");
+const u32 gPokegear_GridMap[] = INCBIN_U32("graphics/pokegear/background.bin.lz");
+
+const u8 gPhoneCall_WindowGfx[] = INCBIN_U8("graphics/pokegear/phone_call.4bpp");
+const u16 gPhoneCall_WindowPal[] = INCBIN_U16("graphics/pokegear/phone_call.gbapal");
+const u32 gPhoneCall_IconGfx[] = INCBIN_U32("graphics/pokegear/phone_call_icon.4bpp.lz");
