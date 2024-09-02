98d957d6cf823ccdf9257b40bd2c4a873df097ff
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-09-02 10:52:42 +0530

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -1,10 +1,10 @@
 #include "../include/global.h"
 
-const u32 gSetClock_Gfx[] = INCBIN_U32("graphics/setclock.4bpp.lz");
-const u16 gSetClock_Pal[] = INCBIN_U16("graphics/setclock.gbapal");
-const u32 gSetClock_Map[] = INCBIN_U32("graphics/setclock.bin.lz");
-const u32 gPokegear_GridMap[] = INCBIN_U32("graphics/background.bin.lz");
+const u32 gSetClock_Gfx[] = INCBIN_U32("graphics/4bpp/setclock.4bpp.lz");
+const u16 gSetClock_Pal[] = INCBIN_U16("graphics/4bpp/setclock.gbapal");
+const u32 gSetClock_Map[] = INCBIN_U32("graphics/4bpp/setclock.bin.lz");
+const u32 gPokegear_GridMap[] = INCBIN_U32("graphics/4bpp/background.bin.lz");
 
-const u8 gPhoneCall_WindowGfx[] = INCBIN_U8("graphics/phone_call.4bpp");
-const u16 gPhoneCall_WindowPal[] = INCBIN_U16("graphics/phone_call.gbapal");
-const u32 gPhoneCall_IconGfx[] = INCBIN_U32("graphics/phone_call_icon.4bpp.lz");
+const u8 gPhoneCall_WindowGfx[] = INCBIN_U8("graphics/4bpp/phone_call.4bpp");
+const u16 gPhoneCall_WindowPal[] = INCBIN_U16("graphics/4bpp/phone_call.gbapal");
+const u32 gPhoneCall_IconGfx[] = INCBIN_U32("graphics/4bpp/phone_call_icon.4bpp.lz");
